37fb8a81f6a1bc2e377c8d46d2eaa6d96e521406
MommyMarioGames <mariogaming2740@proton.me>
2026-03-28 19:45:46 -0600

    Create levels.js
    fetch levels here

---
+++ levels.js
@@ -0,0 +1,26 @@
+const levels = [
+    {
+        id: 1,
+        title: "1. fucking level",
+        creator: "kanye idk",
+        thumbnail: "simpsion.jpeg",
+        video: "zDjpcmvZbTM", // YouTube ID
+        points: 250,
+        position: 1,
+        difficulty: "demon insert here",
+        tags: ["a TAG WOW"],
+        description: "description."
+    },
+    {
+        id: 2,
+        title: "2. another level",
+        creator: "travis scoot",
+        thumbnail: "images/IMG_2258.PNG",
+        video: "glfUnq0rXso",
+        points: 200,
+        position: 2,
+        difficulty: "flightreacts difficulty",
+        tags: ["flight", "reacts"],
+        description: "hey wait is this kanye again?."
+    }
+];
